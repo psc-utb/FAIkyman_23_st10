8d59aa120e43e4e9540ff4be5cbbfa82e52ad7d2
FunTom <vogeltanz@utb.cz>
2023-10-25 11:28:18 +0200

    Dragon AI, Weapon, import assetů
    + Dragon AI -> pohyb a útok
    + dokončení Weapon systému pro hrdinu a draka
    + import health UI a .NET Standard knihovny s kódem hrdiny a draka
    + vytvoření základu scriptu pro použití třídy Postava z knihovny (CharacterBehavior)

---
+++ Assets/Scripts/CharacterBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using hrdina_a_drak.Postavy;
+
+public class CharacterBehavior : MonoBehaviour
+{
+    Postava character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DragonAI.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class DragonAI : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float distanceToAttack = 1;
+
+    [SerializeField]
+    float speed = 2;
+
+    Animator _animatorController;
+    Rigidbody2D _rigidBody2D;
+
+    void Start()
+    {
+        _animatorController = GetComponent<Animator>();
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    bool isAttacking = false;
+    float direction;
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = target.position.x - this.transform.position.x;
+        //Vector2.Distance()
+
+        direction = Mathf.Sign(distance);
+
+        if (Mathf.Abs(distance) < distanceToAttack && isAttacking == false)
+        {
+            _animatorController.SetBool("Moving", false);
+            _animatorController.SetTrigger("Attack");
+            isAttacking = true;
+
+            _rigidBody2D.velocity = new Vector2(0, _rigidBody2D.velocity.y);
+        }
+        else if(isAttacking == false)
+        {
+            _animatorController.SetBool("Moving", true);
+
+            _rigidBody2D.velocity = new Vector2(direction * speed, _rigidBody2D.velocity.y);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (transform.localScale.x < 0
+            && direction > 0
+            ||
+            transform.localScale.x > 0
+            && direction < 0)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+    public void StopAttacking()
+    {
+        isAttacking = false;
+    }
+}
